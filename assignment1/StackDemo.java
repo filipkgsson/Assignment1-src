d33903dde625c47a78f74d2fea4eb2ddb681f6f3
Simon-Cederbom <Simon.cederbom@gmail.com>
2018-03-29 10:50:52 +0200

    Ny Stack

--- assignment1/StackDemo.java
+++ assignment1/StackDemo.java
@@ -2,32 +2,23 @@ package assignment1;
 
 import java.util.*;
 
-public class StackDemo {
+public class StackDemo extends Stack<Integer> {
 
-	private Stack<Integer> stack;
-
-	public StackDemo() {
-		stack = new Stack<Integer>();
-	}
-
-	public void Push(int e) {
-		stack.push(e);
-	}
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = 1L;
 
 	public void Push(int... elements) {
 		for (int e : elements) {
-			stack.push(e);
+			this.push(e);
 		}
-	}
-
-	public int Pop() {
-		return stack.pop();
 	}
 
 	public ArrayList<Integer> Pop(int n) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
-			list.add(0, stack.pop());
+			list.add(0, this.pop());
 		}
 		return list;
 	}
---
+++ assignment1/StackDemoTest.java
@@ -0,0 +1,42 @@
+package assignment1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackDemoTest {
+
+	StackDemo stack1;
+	
+	@Before
+	public void setUp(){
+	stack1 = new StackDemo();
+	}
+
+	@Test
+	public void testStackDemo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPushInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPushIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopInt() {
+		fail("Not yet implemented");
+	}
+
+}
