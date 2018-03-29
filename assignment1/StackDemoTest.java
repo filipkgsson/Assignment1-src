3876e93a8dfdfea4e51fb62d63bb65a96e56a24f
Simon-Cederbom <Simon.cederbom@gmail.com>
2018-03-29 11:14:41 +0200

    JUnit

--- assignment1/StackDemoTest.java
+++ assignment1/StackDemoTest.java
@@ -2,41 +2,36 @@ package assignment1;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class StackDemoTest {
 
-	StackDemo stack1;
+	StackDemo stack;
+	
 	
 	@Before
-	public void setUp(){
-	stack1 = new StackDemo();
+	public void setUp() throws Exception {
+		stack = new StackDemo();
 	}
 
 	@Test
-	public void testStackDemo() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPushInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPushIntArray() {
-		fail("Not yet implemented");
+	public void testPush() {
+		assertTrue(stack.size() == 0); 
+		stack.Push(1,2,3);
+		assertTrue(stack.size() == 3); 
 	}
 
 	@Test
 	public void testPop() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPopInt() {
-		fail("Not yet implemented");
+		stack.Push(1,2,3);
+		ArrayList<Integer> list = stack.Pop(3) ;
+		assertEquals(1 , (int)list.get(0));
+		assertEquals(2 , (int)list.get(1));
+		assertEquals(3 , (int)list.get(2));
+		assertTrue(stack.size() == 0); 
 	}
 
 }
